1c20a9a8a62de10cc62a73e59626154bc5077681
wafflesJ <jacobsamuelmills7@gmail.com>
2025-01-16 20:39:46 -0500

    Update script.js wss

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function addMessage(content, isSelf = false, isImage = false) {
 function setupWebSocket() {
     if (socket) return; // If socket already exists, don't create a new one
 
-    socket = new WebSocket("https://websockets-3ihk.onrender.com:8080"); // Replace with your server URL
+    socket = new WebSocket("wss://websockets-3ihk.onrender.com"); // Replace with your server URL
 
     socket.onopen = () => {
         console.log('WebSocket connection established.');
